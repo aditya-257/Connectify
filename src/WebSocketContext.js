b8575df9c6ff7857ef6ad395feaa7b0f2ef13853
aditya-257 <114898840+aditya-257@users.noreply.github.com>
2024-07-03 22:31:32 +0530

    Update WebSocketContext.js

--- src/WebSocketContext.js
+++ src/WebSocketContext.js
@@ -4,7 +4,7 @@ const WebSocketContext = createContext(null);
 export const WebSocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
     const connect = () => {
-        const ws = io("ws://localhost:5000");
+        const ws = io("ws://connectify16.onrender.com");
         setSocket(ws);
         return ws;
     };
